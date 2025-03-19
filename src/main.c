a398101086968536b674b85afd6869943a8b7d87
oxey <lucoerlemans37@gmail.com>
2025-03-19 11:58:11 +0100

    bit of messing around in the `main.c` file

--- src/main.c
+++ src/main.c
@@ -1,54 +1,48 @@
+// #include "headers/assembler.h"
+
+#include <stdio.h>
+
 #include "headers/cpu.h"
-#include "headers/instructions.h"
 #include "headers/debug.h"
+#include "headers/instructions.h"
+#include "headers/tokenizer.h"
 
 int main(int argc, char** argv) {
     CPU cpu;
     initCpu(&cpu);
 
-    // uint8_t program[] = {
-    //     OP_LOAD_I, 5,
-    //     OP_ADD_I, 3,
-    //     OP_STORE_IM, 0x12, 0x34,
-    //     OP_LOAD_I, 0,
-    //     OP_LOAD_I, 0x12,
-    //     OP_STORE_H,
-    //     OP_LOAD_I, 0x34,
-    //     OP_STORE_L,
-    //     OP_LOAD_MHL,
-    //     OP_HALT
-    // };
-    
-    uint8_t __fibonacci[] = {
+    // clang-format off
+
+    uint8_t fibonacci[] = {
         // init registers with the first two fibonacci values
         OP_LOAD_I, 0,
         OP_STORE_L,
         OP_LOAD_I, 1,
         OP_STORE_H,
-        
+
         // init counter register, we're finding fibbonacci number 5
         OP_LOAD_I, 10,
         OP_STORE_R0,
-        
+
         // Reset accumulator and add both old values to it
         OP_LOAD_I, 0,
         OP_ADD_L,
         OP_ADD_H,
-        
+
         // new is now in accumulator, we need to move H -> L and acc -> H
         OP_XCH_H,
         OP_STORE_L,
-        
+
         // Decrement counter, jump to instruction 9 (start of loop) if not zero
         OP_DEC_R0,
         OP_JNZ, 0, 9,
-        
+
         // Load final value into the accumulator
         OP_LOAD_H,
-        
+
         OP_HALT
     };
-    
+
     uint8_t test[] = {
         OP_ET,
         OP_PUSH_I, 5,
@@ -60,12 +54,32 @@ int main(int argc, char** argv) {
         OP_RET,
     };
 
-    loadProgram(&cpu, test, sizeof(__fibonacci));
-    
-    runCpu(&cpu);
-    
-    printCpu(&cpu);
-    printStack(&cpu, 10);
+    // clang-format on
+
+    // loadProgram(&cpu, fibonacci, sizeof(fibonacci));
+
+    // runCpu(&cpu);
+
+    // printCpu(&cpu);
+    // printStack(&cpu, 10);
+
+    const slice_t program = static_slice("JMP .smie      smie:     ; this is a comment \n LOAD 0 ; this is another comment    \n");
+
+    ProgramLines tokenLines = tokenizeProgram(program);
+    vec_iter_t lineIter = iter_from_vec(&tokenLines.lines);
+
+    TokenLine* line;
+    Token* token;
+    while ((line = iter_next(&lineIter))) {
+        vec_iter_t tokenIter = iter_from_vec(&line->tokens);
+        while ((token = iter_next(&tokenIter))) {
+            printf("tok: ");
+            print_token(token);
+            printf(", str: %.*s, len: %lu\n", (int)token->substr.len, token->substr.str,
+                   token->substr.len);
+        }
+        printf("\n");
+    }
 
     freeCpu(&cpu);
     return 0;
