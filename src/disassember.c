b25a941965fe2ea0f4b5db43f63f64ad6af7c30f
oxey <lucoerlemans37@gmail.com>
2025-04-22 12:26:08 +0200

    Add very small start for a disassembler

---
+++ src/disassember.c
@@ -0,0 +1,19 @@
+#include "headers/disassembler.h"
+
+void disassemble(Executable exec) {
+    // TokenLines tokenLines = tokenizeProgram(program);
+    // vec_iter_t lineIter = iter_from_vec(&tokenLines.lines);
+
+    // TokenLine* line;
+    // Token* token;
+
+    // while ((line = iter_next(&lineIter))) {
+    //     vec_iter_t tokenIter = iter_from_vec(&line->tokens);
+    //     while ((token = iter_next(&tokenIter))) {
+    //         printf("tok: ");
+    //         printToken(token);
+    //         printf(", \"%.*s\"\n", (int)token->substr.len, token->substr.str);
+    //     }
+    //     printf("\n");
+    // }
+}
---
+++ src/headers/disassembler.h
@@ -0,0 +1,8 @@
+#ifndef __OXEY_CCE_DISASSEMBLER_H
+#define __OXEY_CCE_DISASSEMBLER_H
+
+#include "assembler.h"
+
+void disassemble(Executable exec);
+
+#endif
